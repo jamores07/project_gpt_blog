42988137a6feea37a019c6ccb6ca4dc3219a57d9
John Amores <kayak.stated_0b@icloud.com>
2024-07-02 18:49:50 -0400

    Blog - Main WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
-
+import Sidebar from '@/components/Sidebar';
+import Subscribe from '@/components/Subscribe';
 
 export default function Home() {
   return (
-    <>
-    </>
+    <main className="px-10 leading-7">
+      <Trending trendingPosts={trendingPosts} />
+      <div className="md:flex gap-10 mb-5">
+        <div className="basis-3/4">
+          <Tech techPosts={techPosts} />
+          <Travel travelPosts={travelPosts} />
+          <Other otherPosts={otherPosts} />
+          <div className="hidden md:block">
+            <Subscribe />
+          </div>
+        </div>
+        <div className="basis-1/4">
+          <Sidebar />
+        </div>
+      </div>
+    </main>
   );
 }
