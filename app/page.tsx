b46342e585fa7ebc5e7534001316f3233c09ca5b
John Amores <kayak.stated_0b@icloud.com>
2024-07-04 17:45:16 -0400

    Post design WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
 import Sidebar from '@/components/Sidebar';
 import Subscribe from '@/components/Subscribe';
+import { prisma } from '@/app/api/client';
+import { Post } from '@prisma/client';
+
+const getPosts =  async () => {
+  const posts = await prisma.post.findMany();
+
+  const formattedPosts = await Promise.all(
+    posts.map(async (post: Post) => {
+      const imageModule = require(`../public${post.image}`);
+      return {
+        ...post,
+        image: imageModule.default,
+      };
+    })
+  );
+
+  return formattedPosts;
+}
 
 export default function Home() {
+
+
   return (
     <main className="px-10 leading-7">
       <Trending trendingPosts={trendingPosts} />
