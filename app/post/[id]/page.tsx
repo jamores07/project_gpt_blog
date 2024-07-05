eaf26b029ec7ee74645bb5393d3458019419d9f9
John Amores <kayak.stated_0b@icloud.com>
2024-07-05 15:25:28 -0400

    Adding Post editing functionality WIP

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -31,13 +31,25 @@ const getPost = async (id: string) => {
 }
 
 const Post = async ({ params }: Props) => {
-  const { id } = params;
-  const post: FormattedPost | null = await getPost(id)
-
-
-  return (
-    <div>Post</div>
-  )
-}
+    const { id } = params;
+    const post: FormattedPost | null = await getPost(id);
+  
+    if (!post) {
+      return <div>Post Not Found</div>;
+    }
+  
+    return (
+      <main className="px-10 leading-7">
+        <div className="md:flex gap-10 mb-5">
+          <div className="basis-3/4">
+            <Content post={post} />
+          </div>
+          <div className="basis-1/4">
+            <Sidebar />
+          </div>
+        </div>
+      </main>
+    );
+  };
 
 export default Post;
